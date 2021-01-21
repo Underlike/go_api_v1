f357dacf255c64ee143d662422f55c4186070b45
Simon <simon.axilais@gmail.com>
2021-01-21 10:14:32 +0100

    Update: database

--- controllers/apis.go
+++ controllers/apis.go
@@ -1,29 +1,29 @@
 package controllers
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
+
 	"../models"
-	"encoding/json"
 	"github.com/gorilla/mux"
 )
 
 func NewUrl(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("content-type", "application/json")
 	w.WriteHeader(http.StatusOK)
-	
+
 	url := r.FormValue("url")
 
 	var api models.Api
-	models.NewUrl(&api, url)
+
 	fmt.Printf("Insert into database down - OK \n")
-	json.NewEncoder(w).Encode("Url rewrite: " + api.RewriteUrl)
+	json.NewEncoder(w).Encode(models.NewUrl(&api, url))
 }
 
 func RedirectUrl(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
-	url := "http://localhost:5002/" + vars["key"]
-	api := models.SearchUrl(url)
+	api := models.SearchUrl(vars["key"])
 
-	http.Redirect(w, r, api.DefaultUrl, 302)
+	http.Redirect(w, r, api.Url, 302)
 }
--- models/api.go
+++ models/api.go
@@ -1,33 +1,40 @@
 package models
 
 import (
+	"log"
+
 	"../config"
 	"github.com/kare/base62"
 )
 
 type Api struct {
-	Id         int
-	DefaultUrl string
-	RewriteUrl string
+	Id  int
+	Url string
 }
 
 type Apis []Api
 
-func NewUrl(api *Api, url string) **Api {
+func NewUrl(api *Api, url string) *string {
 	config.InitializeDatabase().Order("id desc").Find(&api)
 
 	urlEncode := "http://localhost:5002/" + base62.Encode(int64(api.Id))
-	ap := Api{DefaultUrl: url, RewriteUrl: urlEncode}
+	ap := Api{Url: url}
 
 	config.InitializeDatabase().Create(&ap)
-	config.InitializeDatabase().Raw("SELECT `id`, `default_url`, `rewrite_url` FROM `apis` WHERE `rewrite_url` = ?", urlEncode).Scan(&api)
 
-	return &api
+	return &urlEncode
 }
 
-func SearchUrl(url string) *Api {
+func SearchUrl(key string) *Api {
 	var api Api
-	config.InitializeDatabase().Raw("SELECT `id`, `default_url`, `rewrite_url` FROM `apis` WHERE `rewrite_url` = ?", url).Scan(&api)
+
+	id, err := base62.Decode(key)
+
+	if err != nil {
+		log.Println(err)
+	}
+
+	config.InitializeDatabase().Table("apis").Select("url").Where("id = ?", id).Scan(&api)
 
 	return &api
 }
